958f8b13c4be0c42f1b00dff3defb842128df877
David M. Lee <leedm777@yahoo.com>
2020-11-30 23:19:00 -0600

    Fix type errors

--- src/day01.ts
+++ src/day01.ts
@@ -6,10 +6,11 @@ export function part1(input: string): number {
     .map((s) => parseInt(s, 10))
     .value();
 
-  const [num1, num2] = _(nums)
-    .flatMap((num1) => _.map(nums, (num2) => [num1, num2]))
-    .filter(([num1, num2]) => num1 + num2 === 2020)
-    .head();
+  const [num1, num2] =
+    _(nums)
+      .flatMap((num1) => _.map(nums, (num2) => [num1, num2]))
+      .filter(([num1, num2]) => num1 + num2 === 2020)
+      .head() || [];
 
   return num1 * num2;
 }
@@ -20,11 +21,12 @@ export function part2(input: string): number {
     .map((s) => parseInt(s, 10))
     .value();
 
-  const [num1, num2, num3] = _(nums)
-    .flatMap((num1) => _.map(nums, (num2) => [num1, num2]))
-    .flatMap(([num1, num2]) => _.map(nums, (num3) => [num1, num2, num3]))
-    .filter(([num1, num2, num3]) => num1 + num2 + num3 === 2020)
-    .head();
+  const [num1, num2, num3] =
+    _(nums)
+      .flatMap((num1) => _.map(nums, (num2) => [num1, num2]))
+      .flatMap(([num1, num2]) => _.map(nums, (num3) => [num1, num2, num3]))
+      .filter(([num1, num2, num3]) => num1 + num2 + num3 === 2020)
+      .head() || [];
 
   return num1 * num2 * num3;
 }
