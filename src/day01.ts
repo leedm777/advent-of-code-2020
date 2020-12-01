e1935d7e96ec7a62aa81c4e8cb6c466441d3b5bd
David M. Lee <leedm777@yahoo.com>
2020-12-01 15:27:08 -0600

    Final day 1 optimization
    Really shouldn't spend this long on it, but the lack of filtering using
    `_.combinations()` was really bugging me. Now recursively iterating
    through the combinations myself, filtering early when I know they won't
    work.

--- src/day01.ts
+++ src/day01.ts
@@ -1,16 +1,41 @@
 import _ from "lodash";
-import "lodash.combinations";
 
-export function part1(input: number[], n = 2): number {
+export function part1(
+  input: number[],
+  n = 2,
+  target = 2020
+): number | undefined {
+  // If we've run out of input, there is no match
+  if (_.isEmpty(input)) {
+    return undefined;
+  }
+
+  // If we're looking for a single number, just find it in the input
+  if (n === 1) {
+    if (_.includes(input, target)) {
+      return target;
+    }
+    return undefined;
+  }
+
+  // We're looking for n numbers whose sum is target. Since all numbers are
+  // positive, we can optimize a bit.
   return (
     _(input)
-      .combinations(n)
-      .filter((nums) => _.sum(nums) === 2020)
-      .map((nums) => _.reduce(nums, _.multiply))
-      .head() || NaN
+      // filter out any numbers that are too large
+      .filter((num) => num < target)
+      // For `num` to be a solution for `target`, the remaining array has to be
+      // a solution for `target - num`.
+      .map((num, idx) => {
+        const rem = _.drop(input, idx + 1);
+        const product = part1(rem, n - 1, target - num);
+        return product && product * num;
+      })
+      // and find the first solution
+      .find((num) => !_.isNil(num))
   );
 }
 
-export function part2(input: number[]): number {
+export function part2(input: number[]): number | undefined {
   return part1(input, 3);
 }
