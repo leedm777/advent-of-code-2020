cfef2371f6b8dcae17ab68d55be49696ba5d5ee2
David M. Lee <leedm777@yahoo.com>
2020-12-02 23:28:00 -0600

    Day 3 solution
    Lost a lot of time on part 2; I forgot to pass the slope as a param to
    the recursive call when I moved slope to being a param.
    
    Speaking of recursion, I should brush up on how to convert recusion to
    loops, since at some point one of my solutions is going to blow the
    stack :-/

--- src/day03.spec.ts
+++ src/day03.spec.ts
@@ -6,8 +6,23 @@ const puzzleInput = splitLines(readFile("./src/day03.txt"));
 describe("day 3", () => {
   describe("part 1", () => {
     it.each([
-      [[], 0],
-      [puzzleInput, 0],
+      [
+        [
+          "..##.......",
+          "#...#...#..",
+          ".#....#..#.",
+          "..#.#...#.#",
+          ".#...##..#.",
+          "..#.##.....",
+          ".#.#.#....#",
+          ".#........#",
+          "#.##...#...",
+          "#...##....#",
+          ".#..#...#.#",
+        ],
+        7,
+      ],
+      [puzzleInput, 282],
     ])("input %#", (input, expected) => {
       const actual = part1(input);
       expect(actual).toStrictEqual(expected);
@@ -16,8 +31,23 @@ describe("day 3", () => {
 
   describe("part 2", () => {
     it.each([
-      [[], 0],
-      [puzzleInput, 0],
+      [
+        [
+          "..##.......",
+          "#...#...#..",
+          ".#....#..#.",
+          "..#.#...#.#",
+          ".#...##..#.",
+          "..#.##.....",
+          ".#.#.#....#",
+          ".#........#",
+          "#.##...#...",
+          "#...##....#",
+          ".#..#...#.#",
+        ],
+        336,
+      ],
+      [puzzleInput, 958815792],
     ])("input %#", (input, expected) => {
       const actual = part2(input);
       expect(actual).toStrictEqual(expected);
--- src/day03.ts
+++ src/day03.ts
@@ -1,9 +1,41 @@
 import _ from "lodash";
 
-export function part1(input: string[]): number {
-  return _.sum(input);
+/**
+ * Starting at the top-left corner of your map and following a slope of right 3
+ * and down 1, how many trees would you encounter?
+ */
+export function part1(
+  input: string[],
+  [x, y] = [0, 0],
+  numTrees = 0,
+  slope = [3, 1]
+): number {
+  if (y >= input.length) {
+    return numTrees;
+  }
+
+  const square = input[y][x % input[y].length];
+  if (square === "#") {
+    ++numTrees;
+  }
+
+  return part1(input, [x + slope[0], y + slope[1]], numTrees, slope);
 }
 
+/**
+ * What do you get if you multiply together the number of trees encountered on
+ * each of the listed slopes?
+ */
 export function part2(input: string[]): number {
-  return _.sum(input);
+  const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ];
+
+  return _(slopes)
+    .map((slope) => part1(input, [0, 0], 0, slope))
+    .reduce(_.multiply, 1);
 }
