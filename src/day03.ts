32f8474cd35b5c9db09aa2be9e002bf86a633f24
David M. Lee <leedm777@yahoo.com>
2020-12-02 23:32:47 -0600

    Went ahead and un-recursed day 3

--- src/day03.ts
+++ src/day03.ts
@@ -4,22 +4,20 @@ import _ from "lodash";
  * Starting at the top-left corner of your map and following a slope of right 3
  * and down 1, how many trees would you encounter?
  */
-export function part1(
-  input: string[],
-  [x, y] = [0, 0],
-  numTrees = 0,
-  slope = [3, 1]
-): number {
-  if (y >= input.length) {
-    return numTrees;
-  }
+export function part1(input: string[], slope = [3, 1]): number {
+  let [x, y] = [0, 0];
+  let numTrees = 0;
 
-  const square = input[y][x % input[y].length];
-  if (square === "#") {
-    ++numTrees;
-  }
+  while (y < input.length) {
+    const square = input[y][x % input[y].length];
+    if (square === "#") {
+      ++numTrees;
+    }
 
-  return part1(input, [x + slope[0], y + slope[1]], numTrees, slope);
+    x += slope[0];
+    y += slope[1];
+  }
+  return numTrees;
 }
 
 /**
@@ -36,6 +34,6 @@ export function part2(input: string[]): number {
   ];
 
   return _(slopes)
-    .map((slope) => part1(input, [0, 0], 0, slope))
+    .map((slope) => part1(input, slope))
     .reduce(_.multiply, 1);
 }
