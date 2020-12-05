123355b49d5aed653d604758415e1e3ea0ac82cf
David M. Lee <leedm777@yahoo.com>
2020-12-05 09:14:58 -0600

    Day 5 solution

---
+++ src/day05.spec.ts
@@ -0,0 +1,43 @@
+import { part1, part2, computeSeatId } from "./day05";
+import { readFile, splitLines } from "./aoc";
+
+const puzzleInput = splitLines(readFile("./src/day05.txt"));
+
+describe("day 5", () => {
+  describe("computeSeatId", () => {
+    it.each([
+      /*
+      Here are some other boarding passes:
+        BFFFBBFRRR: row 70, column 7, seat ID 567.
+        FFFBBBFRRR: row 14, column 7, seat ID 119.
+        BBFFBBFRLL: row 102, column 4, seat ID 820.
+       */
+      ["FBFBBFFRLR", 357],
+      // FBFBBFF RLR
+      // 0101100 101,
+      ["BFFFBBFRRR", 567],
+      ["FFFBBBFRRR", 119],
+      ["BBFFBBFRLL", 820],
+    ])("input %#", (input, expected) => {
+      const actual = computeSeatId(input);
+      expect(actual).toStrictEqual(expected);
+    });
+  });
+
+  describe("part 1", () => {
+    it.each([
+      [["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"], 820],
+      [puzzleInput, 890],
+    ])("input %#", (input, expected) => {
+      const actual = part1(input);
+      expect(actual).toStrictEqual(expected);
+    });
+  });
+
+  describe("part 2", () => {
+    it.each([[puzzleInput, 651]])("input %#", (input, expected) => {
+      const actual = part2(input);
+      expect(actual).toStrictEqual(expected);
+    });
+  });
+});
---
+++ src/day05.ts
@@ -0,0 +1,47 @@
+import _ from "lodash";
+
+/*
+The first 7 characters will either be F or B; these specify exactly one of the
+128 rows on the plane (numbered 0 through 127). Each letter tells you which half
+ of a region the given seat is in. Start with the whole list of rows; the first
+ letter indicates whether the seat is in the front (0 through 63) or the back
+ (64 through 127). The next letter indicates which half of that region the seat
+  is in, and so on until you're left with exactly one row.
+
+
+The last three characters will be either L or R; these specify exactly one of
+ the 8 columns of seats on the plane (numbered 0 through 7). The same process
+  as above proceeds again, this time with only three steps. L means to keep the
+   lower half, while R means to keep the upper half.
+ */
+export function computeSeatId(code: string): number {
+  return (
+    _(code)
+      // TODO: useless map fixes "Property 'reduce' does not exist on type
+      //       'String'."; code runs fine w/o
+      .map((x) => x)
+      .reduce((id: number, ch: string) => {
+        let nextCode = id << 1;
+        if (ch === "B" || ch === "R") {
+          nextCode = nextCode | 1;
+        }
+        return nextCode;
+      }, 0)
+  );
+}
+
+export function part1(input: string[]): number | undefined {
+  return _(input).map(computeSeatId).max();
+}
+
+export function part2(input: string[]): number {
+  const seats = _(input).map(computeSeatId).sortBy().value();
+  let prior = seats[0];
+  for (let i = 1; i < seats.length; ++i) {
+    if (seats[i] !== prior + 1) {
+      return prior + 1;
+    }
+    prior = seats[i];
+  }
+  return NaN;
+}
