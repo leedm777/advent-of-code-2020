634903a1cebb92e142fecb501ab07c6b12b91163
David M. Lee <leedm777@yahoo.com>
2020-12-05 23:18:23 -0600

    Solution for day 6

--- src/day06.spec.ts
+++ src/day06.spec.ts
@@ -1,24 +1,53 @@
-import { part1, part2 } from "./day06";
-import { readFile, splitLines } from "./aoc";
+import {
+  part1,
+  part2,
+  countUnionAnswers,
+  countIntersectionAnswers,
+} from "./day06";
+import { readFile, splitChunks } from "./aoc";
 
-const puzzleInput = splitLines(readFile("./src/day06.txt"));
+const puzzleInput = splitChunks(readFile("./src/day06.txt"));
 
 describe("day06", () => {
-  describe("part 1", () => {
+  describe("countAnswers", () => {
     it.each([
-      [[], 0],
-      [puzzleInput, 0],
+      ["", 0],
+      ["abcx\nabcy\nabcz", 6],
+      ["abc", 3],
+      ["a\nb\nc", 3],
+      ["ab\nac", 3],
+      ["a\na\na\na", 1],
+      ["b", 1],
     ])("input %#", (input, expected) => {
+      const actual = countUnionAnswers(input);
+      expect(actual).toStrictEqual(expected);
+    });
+  });
+
+  describe("part 1", () => {
+    it.each([[puzzleInput, 6878]])("input %#", (input, expected) => {
       const actual = part1(input);
       expect(actual).toStrictEqual(expected);
     });
   });
 
-  describe("part 2", () => {
+  describe("countCommonAnswers", () => {
     it.each([
-      [[], 0],
-      [puzzleInput, 0],
+      ["", 0],
+      ["abcx\nabcy\nabcz", 3],
+      ["abc", 3],
+      ["a\nb\nc", 0],
+      ["ab\nac", 1],
+      ["a\na\na\na", 1],
+      ["b", 1],
     ])("input %#", (input, expected) => {
+      const actual = countIntersectionAnswers(input);
+      expect(actual).toStrictEqual(expected);
+    });
+  });
+
+  describe("part 2", () => {
+    it.each([[puzzleInput, 3464]])("input %#", (input, expected) => {
       const actual = part2(input);
       expect(actual).toStrictEqual(expected);
     });
--- src/day06.ts
+++ src/day06.ts
@@ -1,9 +1,26 @@
 import _ from "lodash";
+import { splitLines } from "./aoc";
+
+export function countUnionAnswers(input: string): number {
+  return _(input)
+    .filter((s) => /[a-z]/.test(s))
+    .uniq()
+    .size();
+}
 
 export function part1(input: string[]): number {
-  return _.sum(input);
+  return _(input).map(countUnionAnswers).sum();
+}
+
+export function countIntersectionAnswers(input: string): number {
+  const answers = _(input)
+    .thru(splitLines)
+    .map((s) => _.split(s, ""))
+    .value();
+  const commonAnswers = _.intersection(...answers);
+  return commonAnswers.length;
 }
 
 export function part2(input: string[]): number {
-  return _.sum(input);
+  return _(input).map(countIntersectionAnswers).sum();
 }
