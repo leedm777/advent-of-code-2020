ff44aa7b56b4692dfaa0d668935712339d74a962
David M. Lee <leedm777@yahoo.com>
2020-12-13 13:17:20 -0600

    Really sloppy solution to day 13

--- src/day13.spec.ts
+++ src/day13.spec.ts
@@ -1,16 +1,9 @@
-import { part1, part2, helperOfTheDay } from "./day13";
+import { part1, part2 } from "./day13";
 import { readFile, splitLines } from "./aoc";
 
 const puzzleInput = splitLines(readFile("./src/day13.txt"));
 
 describe("day13", () => {
-  describe("helperOfTheDay", () => {
-    it.each([[[], 0]])("input %#", (input, expected) => {
-      const actual = helperOfTheDay(input);
-      expect(actual).toStrictEqual(expected);
-    });
-  });
-
   describe("part 1", () => {
     it.each([
       [["939", "7,13,x,x,59,x,31,19"], 295],
@@ -24,12 +17,13 @@ describe("day13", () => {
   describe("part 2", () => {
     it.each([
       [["939", "7,13,x,x,59,x,31,19"], 1068781],
+      [["", "17,x,13"], 102],
       [["", "17,x,13,19"], 3417],
       [["", "67,7,59,61"], 754018],
       [["", "67,x,7,59,61"], 779210],
       [["", "67,7,x,59,61"], 1261476],
       [["", "1789,37,47,1889"], 1202161486],
-      // [puzzleInput, 0],
+      [puzzleInput, 600689120448303],
     ])("input %#", (input, expected) => {
       const actual = part2(input);
       expect(actual).toStrictEqual(expected);
--- src/day13.ts
+++ src/day13.ts
@@ -1,78 +1,132 @@
 import _ from "lodash";
-import { MinHeap, SideLogger } from "./aoc";
 
-export function helperOfTheDay(input: string[]): number {
-  return 0;
-}
+const log = { log: _.noop, clear: _.noop };
+// new SideLogger("day13.log");
+log.clear();
 
 export function part1([earliestStr, busIdStr]: string[]): number {
   const earliest = parseInt(earliestStr, 10);
-  const busIds = _(busIdStr)
+  return _(busIdStr)
     .split(",")
     .filter((id) => id !== "x")
-    .map((i) => parseInt(i, 10));
-
-  const waitTimes = busIds.map((id) => [id, id - (earliest % id)]);
-  const minWaitTime = waitTimes.map(([, waitTime]) => waitTime).min();
-  const [busId, waitTime] = waitTimes.find(
-    ([, waitTime]) => waitTime === minWaitTime
-  ) as [number, number];
-  return busId * waitTime;
+    .map((i) => parseInt(i, 10))
+    .map((id) => [id, id - (earliest % id)])
+    .thru((waitTimes) => {
+      const minWaitTime = _(waitTimes)
+        .map(([, waitTime]) => waitTime)
+        .min();
+      const [busId, waitTime] = _.find(
+        waitTimes,
+        ([, waitTime]) => waitTime === minWaitTime
+      ) as [number, number];
+      return busId * waitTime;
+    })
+    .value();
 }
+
+// interface BruteForceBus {
+//   id: number;
+//   offset: number;
+//   nextArrival: number;
+// }
+//
+// export function part2BruteForce([, idStr]: string[]): number {
+//   const schedule = new MinHeap<BruteForceBus>();
+//
+//   function reschedule(bus: BruteForceBus) {
+//     bus.nextArrival = bus.nextArrival + bus.id;
+//     schedule.insert(bus.nextArrival, bus);
+//   }
+//
+//   let firstBus: Bus | null = null;
+//
+//   _(idStr)
+//     .split(",")
+//     .map((i, offset) => {
+//       const id = parseInt(i, 10);
+//       return { offset, id, nextArrival: id };
+//     })
+//     .filter(({ id }) => !_.isNaN(id))
+//     .tap(([b]) => (firstBus = b))
+//     .forEach((bus) => schedule.insert(bus.nextArrival, bus));
+//
+//   if (!firstBus) {
+//     return NaN;
+//   }
+//
+//   const isSolved = (): boolean => {
+//     const next = schedule.peek() as BruteForceBus;
+//     if (next.id !== firstBus.id) {
+//       return false;
+//     }
+//     const t = next.nextArrival;
+//     return _.every(schedule.heap, ({ node }: { node: BruteForceBus }) => {
+//       const good = node.nextArrival - t === node.offset;
+//       // log.log(
+//       //   `  ${node.id}: (${node.nextArrival} - ${t} === ${node.offset}) === ${good}`
+//       // );
+//       return good;
+//     });
+//   };
+//
+//   while (!isSolved()) {
+//     const next = schedule.extract() as BruteForceBus;
+//     log.log(`${next.nextArrival}: ${next.id}`);
+//     reschedule(next);
+//   }
+//
+//   return schedule.peek().nextArrival;
+// }
 
 interface Bus {
-  busId: number;
+  id: number;
   offset: number;
-  nextArrival: number;
 }
 
-const log = { log: _.noop, clear: _.noop }; //new SideLogger("day13.log");
-log.clear();
+function findFirst(
+  k: number,
+  mod: number,
+  { id, offset }: Bus
+): { k: number; mod: number } {
+  let n = 0;
+  let rem = (id - offset) % id;
+  if (rem < 0) {
+    rem += id;
+  }
+  log.log(JSON.stringify({ k, mod, id, offset }));
+  while ((k + mod * n) % id !== rem) {
+    log.log("  " + JSON.stringify({ n, t: k + mod * n, rem }));
+    ++n;
+  }
+
+  log.log("  " + JSON.stringify({ n, t: k + mod * n, rem }));
+  log.log("    " + JSON.stringify({ k, mod, n }));
+
+  return { mod: mod * id, k: k + mod * n };
+}
 
 export function part2([, busIdStr]: string[]): number {
-  const schedule = new MinHeap<Bus>();
-
-  function reschedule(bus: Bus) {
-    bus.nextArrival = bus.nextArrival + bus.busId;
-    schedule.insert(bus.nextArrival, bus);
-  }
-
-  let firstBus: Bus | null = null;
-
-  _(busIdStr)
+  const buses: Bus[] = _(busIdStr)
     .split(",")
     .map((i, offset) => {
-      const busId = parseInt(i, 10);
-      return { offset, busId, nextArrival: busId };
+      const id = parseInt(i, 10);
+      return { offset, id };
     })
-    .filter(({ busId }) => !_.isNaN(busId))
-    .tap(([b]) => (firstBus = b))
-    .forEach((bus) => schedule.insert(bus.nextArrival, bus));
+    .filter(({ id }) => !_.isNaN(id))
+    .value();
 
-  if (!firstBus) {
-    return NaN;
+  // we now have a system of equations we want to solve, where
+  // bus[n].id - t % bus[n].id === bus[n].offset
+  // but n could be so large we can't brute force it
+
+  // Math-ish would be (t = (bus[n].id - bus[n].offset)) mod bus[n].id
+  let k = 0;
+  let mod = 1;
+  let bus: Bus | undefined;
+  while ((bus = buses.shift())) {
+    ({ k, mod } = findFirst(k, mod, bus));
+    log.log(JSON.stringify({ mod, k }));
   }
 
-  const isSolved = (): boolean => {
-    const next = schedule.peek() as Bus;
-    if (next.busId !== firstBus.busId) {
-      return false;
-    }
-    const t = next.nextArrival;
-    return _.every(schedule.heap, ({ node }: { node: Bus }) => {
-      const good = node.nextArrival - t === node.offset;
-      log.log(
-        `  ${node.busId}: (${node.nextArrival} - ${t} === ${node.offset}) === ${good}`
-      );
-      return good;
-    });
-  };
-
-  while (!isSolved()) {
-    const next = schedule.extract() as Bus;
-    log.log(`${next.nextArrival}: ${next.busId}`);
-    reschedule(next);
-  }
-
-  return schedule.peek().nextArrival;
+  return k;
 }
